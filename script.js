06cd427b39dc1b4fb149c04c5b30599dc40c5686
gercamjr <gercamjr.dev@gmail.com>
2022-05-18 13:19:46 -0500

    added three new sounds

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong', 'clubmusic', 'drumhype', 'hornhype']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
